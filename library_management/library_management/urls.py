b4d2bc0073305337a37515d84c146f5063e77c42
Nilesh5168 <128348098+Nilesh5168@users.noreply.github.com>
2025-03-23 16:14:51 +0530

    Delete library_management directory

--- library_management/library_management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Include app URLs
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('library_app.urls')),  # Include the app's URLs
-]
